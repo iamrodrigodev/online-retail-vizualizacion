8dd605f5190187c6bda7787c6c5394aea09224b2
Rodrigo Infanzon Acosta <rinfanzona@ulasalle.edu.pe>
2025-11-08 14:20:21 -0500

    update

--- core/settings.py
+++ core/settings.py
@@ -6,7 +6,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get('SECRET_KEY', default='your-secret-key')
 
-DEBUG = os.environ.get('DEBUG') == 'TRUE'
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
